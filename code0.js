47edbdf08905c25f90a69f0b51d5211bb2d14c19
Tommy Filliater <527274+evolsoulx@users.noreply.github.com>
2021-04-27 10:58:23 -0400

    a

---
+++ code0.js
@@ -0,0 +1,33 @@
+gdjs.New_32sceneCode = {};
+gdjs.New_32sceneCode.GDNewObjectObjects1= [];
+gdjs.New_32sceneCode.GDNewObjectObjects2= [];
+
+gdjs.New_32sceneCode.conditionTrue_0 = {val:false};
+gdjs.New_32sceneCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.New_32sceneCode.eventsList0 = function(runtimeScene) {
+
+{
+
+
+{
+}
+
+}
+
+
+};
+
+gdjs.New_32sceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.New_32sceneCode.GDNewObjectObjects1.length = 0;
+gdjs.New_32sceneCode.GDNewObjectObjects2.length = 0;
+
+gdjs.New_32sceneCode.eventsList0(runtimeScene);
+return;
+
+}
+
+gdjs['New_32sceneCode'] = gdjs.New_32sceneCode;
