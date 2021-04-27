cbf57897bc22597f2d5bbb266d8fd815ece28861
Tommy Filliater <527274+evolsoulx@users.noreply.github.com>
2021-04-27 11:29:17 -0400

    a

--- code0.js
+++ code0.js
@@ -1,9 +1,12 @@
 gdjs.New_32sceneCode = {};
 gdjs.New_32sceneCode.GDNewObjectObjects1= [];
 gdjs.New_32sceneCode.GDNewObjectObjects2= [];
+gdjs.New_32sceneCode.GDClickTextObjects1= [];
+gdjs.New_32sceneCode.GDClickTextObjects2= [];
 
 gdjs.New_32sceneCode.conditionTrue_0 = {val:false};
 gdjs.New_32sceneCode.condition0IsTrue_0 = {val:false};
+gdjs.New_32sceneCode.condition1IsTrue_0 = {val:false};
 
 
 gdjs.New_32sceneCode.eventsList0 = function(runtimeScene) {
@@ -11,8 +14,16 @@ gdjs.New_32sceneCode.eventsList0 = function(runtimeScene) {
 {
 
 
+gdjs.New_32sceneCode.condition0IsTrue_0.val = false;
 {
+gdjs.New_32sceneCode.condition0IsTrue_0.val = gdjs.evtTools.input.isScrollingUp(runtimeScene);
+}if (gdjs.New_32sceneCode.condition0IsTrue_0.val) {
+gdjs.copyArray(runtimeScene.getObjects("ClickText"), gdjs.New_32sceneCode.GDClickTextObjects1);
+{runtimeScene.getVariables().getFromIndex(0).add(1);
+}{for(var i = 0, len = gdjs.New_32sceneCode.GDClickTextObjects1.length ;i < len;++i) {
+    gdjs.New_32sceneCode.GDClickTextObjects1[i].setString("test" + gdjs.evtTools.common.toString(gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(0))));
 }
+}}
 
 }
 
@@ -24,6 +35,8 @@ runtimeScene.getOnceTriggers().startNewFrame();
 
 gdjs.New_32sceneCode.GDNewObjectObjects1.length = 0;
 gdjs.New_32sceneCode.GDNewObjectObjects2.length = 0;
+gdjs.New_32sceneCode.GDClickTextObjects1.length = 0;
+gdjs.New_32sceneCode.GDClickTextObjects2.length = 0;
 
 gdjs.New_32sceneCode.eventsList0(runtimeScene);
 return;
